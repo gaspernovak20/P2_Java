48e207fc599139536f04531243567b980627e3b7
Gasper Novak <gaspernovak22@gmail.com>
2024-03-01 22:15:09 +0100

    DN01 in nekaj K1

--- src/DN01.java
+++ src/DN01.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class DN01 {
+public class DN01 {
+
+    public static void main(String[] args) {
+        System.out.println("Moje ime je: Gašper Novak");
+        System.out.println("Moja vpisna številka: 63220232");
+        System.out.println();
+        System.out.println("***********************************************");
+        System.out.println("*|*  Izjavljam,  da bom vse obveznosti pri  *|*");
+        System.out.println("*|*  Programiranju 2 opravil/a samostojno.  *|*");
+        System.out.println("***********************************************");
+    }
 }
--- src/K01.java
+++ src/K01.java
@@ -1,13 +1,13 @@
 public class K01 {
 
-    static void java() {
+    public static void java() {
         System.out.println("   J    a   v     v  a ");
         System.out.println("   J   a a   v   v  a a");
         System.out.println("J  J  aaaaa   V V  aaaaa");
         System.out.println(" JJ  a     a   V  a     a");
     }
 
-    static void kalkulator(int a, int b) {
+    public static void kalkulator(int a, int b) {
 
         if (b == 0) {
             System.out.println("Napaka: deljenje z 0");
@@ -21,8 +21,25 @@ public class K01 {
 
     }
 
+
+//    public static void nicli (int a, int b , int c){
+//
+//    }
+
+    public static void krog(double r, int d) {
+        if (r <= 0) {
+            System.out.println("Napaka: negativen polmer");
+        } else if (d <= 0) {
+            System.out.println("Napaka: negativen d");
+        } else {
+            System.out.printf("Obseg kroga s polmerom r=%f je %.3f%n", r, (2 * Math.PI * r));
+            System.out.printf("Ploscina kroga s polmerom r=%f je %.3f%n", r, (Math.PI * r * r));
+        }
+    }
+
     public static void main(String[] args) {
         java();
         kalkulator(1, 1);
+        krog(3.2, 4);
     }
 }
